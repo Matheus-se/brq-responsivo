c6e6b076028e8d07444b857c2b1af81f331a2ab8
miguelmendes44 <miguel.mendes@bitzen.com.br>
2020-10-24 19:11:32 -0300

    Initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="" variant="">
+      <b-navbar-brand href="#">
+        <img src="/images/logo-brq.png" class="logo" alt="" />
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">Quem Somos</b-nav-item>
+          <b-nav-item href="#">Como Transformamos</b-nav-item>
+          <b-nav-item href="#">Conteúdos</b-nav-item>
+          <b-nav-item href="#">RI</b-nav-item>
+          <b-nav-item href="#">Venha ser Fera</b-nav-item>
+          <b-nav-item href="#">Fale com a gente</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-0">
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+    <Nuxt />
+    
+  </div>
+</template>
+
+<style lang="scss">
+
+.logo {
+  height: 85px;
+}
+
+.navbar-nav .nav-link {
+  padding-left: 25px !important;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,340 @@
+<template>
+  <div>
+    <div class="db-1"></div>
+    <!--background-->
+    <section class="title">
+      <div class="container">
+        <div class="row">
+          <div class="col my-md-5 my-3">
+            <h1>Sua parceira em todos os desafios da transformação digital</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4 col-10">
+            <p>
+              A BRQ atua no ciclo completo de desenvolvimento, desde a concepção
+              até a materialização dos produtos digitais.
+            </p>
+            <img src="/images/arrow-dobra.svg" class="mt-3 d-none d-md-block" />
+            <h2 class="method mt-5 d-none d-md-block">
+              Conheça a<br />
+              nossa metodologia
+            </h2>
+          </div>
+          <div class="col-12 col-md-8">
+            <b-img src="https://picsum.photos/300/150/?image=41" fluid-grow alt="Fluid-grow image"></b-img>
+            <h2 class="method mt-5 d-block d-sm-none ml-auto">
+              Conheça a<br />
+              nossa metodologia
+            </h2>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="main-content mt-5">
+      <!--<b-container fluid no-padding>
+        <b-row align-v="center" no-gutters>
+          <b-col class="left-content" cols="6">
+            <h1>Como geramos valor aos nossos clientes?</h1>
+            <div class="box-subtitle">
+              Atuamos no ciclo completo de desenvolvimento, desde a concepção
+              até a materialização dos produtos digitais.
+            </div>
+          </b-col>
+          <b-col class="right-content" cols="6">
+            <div class="content-box">
+              <h2>Soluções Inovadoras</h2>
+              <p>
+                Conectar clientes com soluçoes proprietárias disruptivas que
+                acelerem a transformação digital ou criar novos modelos de
+                negócios.
+              </p>
+            </div>
+
+            <div class="content-box">
+              <h2>Transformação digital</h2>
+              <p>
+                Transformar negócios com tecnologia evoluindoe acelerando como
+                as empresas operame entregam valor para seus
+                clientes.Digitalização e aprendizado contínuopara melhoria dos
+                negócios dos clientes.
+              </p>
+            </div>
+
+            <div class="content-box">
+              <h2>Squads Gerenciados</h2>
+              <p>
+                Times cross-funcionais, ágeis e auto gerenciáveis, com alta
+                performance e estratégia orientada à entrega de Valor.
+              </p>
+            </div>
+
+            <div class="content-box">
+              <h2>Times especializados</h2>
+              <p>
+                Times multidisciplinares com alta capacidade técnica que apoiam
+                o desenvolvimento e melhoria contínua de produtos digitais.
+              </p>
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>-->
+
+      <div class="content-box cover-1">
+        <div class="box-image" style="
+            background-image: url(https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg);
+          ">
+          &nbsp;
+        </div>
+        <div class="content container">
+          <h2>Soluções Inovadoras</h2>
+          <p>
+            Conectar clientes com soluçoes proprietárias disruptivas que
+            acelerem a transformação digital ou criar novos modelos de negócios.
+          </p>
+          <div class="cover">&nbsp;</div>
+        </div>
+      </div>
+
+      <div class="content-box cover-2">
+        <div class="box-image"
+          style="background-image: url(https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg);">&nbsp;
+        </div>
+        <div class="content container">
+          <h2>Transformação digital</h2>
+          <p>
+            Transformar negócios com tecnologia evoluindoe acelerando como as
+            empresas operame entregam valor para seus clientes.Digitalização e
+            aprendizado contínuopara melhoria dos negócios dos clientes.
+          </p>
+          <div class="cover">&nbsp;</div>
+        </div>
+      </div>
+
+      <div class="content-box cover-3">
+        <div class="box-image" style="background-image: url(https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg);
+          ">
+          &nbsp;
+        </div>
+        <div class="content container">
+          <h2>Squads Gerenciados</h2>
+          <p>
+            Times cross-funcionais, ágeis e auto gerenciáveis, com alta
+            performance e estratégia orientada à entrega de Valor.
+          </p>
+          <div class="cover">&nbsp;</div>
+        </div>
+      </div>
+      <!--end of box-3-->
+    </section>
+
+    <section class="about-team">
+      <div class="be-brq">
+        <div class="container h-100">
+          <div class="row h-100">
+            <div class="col-12 col-md-8 title d-flex align-items-start flex-column h-100">
+              <h1>Paixão por transformar negócios com tecnologia</h1>
+              <img src="/images/seals-gptw.png" class="mt-auto" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container context">
+        <div class="row">
+          <div class="col-md-7 offset-md-5">
+            <p>São nossos feras que traduzem a nossa essência para o mercado e trabalham engajados para entregar a
+              melhor experiência para nossos clientes todos os dias.</p>
+            <p class="mt-4">Aqui, <strong>respeito e diversidade</strong> importam, porque cada um, do seu jeito, nos
+              ajuda a evoluir
+              para alcançar nosso Norte Verdadeiro em uma <strong>Cultura Lean e Ágil</strong>.</p>
+            <a href="#" class="btn btn-primary btn-lg mt-4">
+              Venha ser um #FERABRQ
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right" fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+
+  export default Vue.extend({});
+</script>
+
+<style lang="scss">
+  /* Diagonal Boxes - */
+
+  .db-1 {
+    background: linear-gradient(#25242a, #121116);
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 70%);
+    position: absolute;
+    top: 0;
+    width: 100vw;
+    height: 110vh;
+    z-index: -1;
+  }
+
+  .title {
+    .method {
+      text-transform: uppercase;
+      padding: 5%;
+      border-left: 1px solid var(--dark);
+      color: var(--brq-yellow);
+      font-weight: medium;
+    }
+  }
+
+  .main-content {
+    .left-content {
+      h1 {
+        font-size: 4rem;
+        text-transform: uppercase;
+        font-weight: bolder;
+      }
+
+      .box-subtitle {
+        background: var(--brq-blue);
+        font-size: 1.5rem;
+        padding: 3rem;
+        color: var(--white);
+      }
+    }
+
+    .content-box {
+      background: linear-gradient(#25242a, #121116);
+      color: var(--white);
+      text-align: right;
+      position: relative;
+      height: 250px;
+
+      .content {
+        padding: 4% 0 4% 53vw;
+      }
+
+      .cover {
+        background: var(--white);
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        clip-path: polygon(0 0, 100% 0%, 95% 100%, 0 100%);
+        transition: opacity 0.5s ease-in-out;
+      }
+
+      .box-image {
+        position: absolute;
+        clip-path: polygon(0 0, 100% 0%, 95% 100%, 0 100%);
+        height: 100%;
+        background-size: cover;
+        background-position: center center;
+      }
+    }
+
+    .content-box:hover {
+      .cover {
+        opacity: 0;
+      }
+    }
+
+    .cover-1 {
+
+      .box-image,
+      .cover {
+        width: 60%;
+      }
+    }
+
+    .cover-2 {
+
+      .box-image,
+      .cover {
+        width: 57%;
+      }
+    }
+
+    .cover-3 {
+
+      .box-image,
+      .cover {
+        width: 54.1%;
+      }
+    }
+
+    h2 {
+      color: var(--brq-yellow);
+      text-transform: uppercase;
+      font-weight: bolder;
+    }
+  }
+
+  .about-team {
+    .context {
+      font-size: 1.4rem;
+      margin-top: -50px;
+      padding-bottom: 7rem;
+
+      strong {
+        color: var(--brq-darkblue);
+      }
+
+      svg {
+        margin-left: 3rem;
+        font-size: 2rem;
+      }
+
+      .btn-primary {
+        background: var(--brq-darkblue);
+        border: 0;
+        border-radius: 0;
+        padding: 1.1rem 2rem;
+      }
+    }
+  }
+
+  .be-brq {
+    height: 110vh;
+    background: url(/images/bg-trabalhe-conosco.jpg) center center/cover;
+    clip-path: polygon(0 0, 100% 0, 100% 70%, 0 100%);
+
+    .title {
+      padding-top: 6rem;
+      padding-bottom: 5rem;
+    }
+  }
+
+  @media (max-width: 575.98px) {
+    .db-1 {
+      height: 130vw;
+      clip-path: polygon(0 0, 100% 0, 100% 100%, 0 90%);
+    }
+
+    .about-team {
+      .context {
+        margin-top: 2rem;
+      }
+    }
+
+    .be-brq {
+      height: 60vh;
+      clip-path: polygon(0 0, 100% 0, 100% 90%, 0 100%);
+
+      .title {
+        padding-top: 2rem;
+        padding-bottom: 3rem;
+      }
+
+      img {
+        width: 50%;
+      }
+
+    }
+  }
+</style>
