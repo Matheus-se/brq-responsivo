f15da95c4ce2969cfd9a1d7339cae08b6736fd3e
Matheus Henrique <matheus.santos@bitzen.com.br>
2020-11-10 16:00:41 -0300

    blog changes

--- pages/blog.vue
+++ pages/blog.vue
@@ -30,183 +30,51 @@
         </div>
       </div>
     </section>
-
-    <section class="content py-5 py-md-3">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-9 main-box pr-md-5">
-            <p>
-              Olá, meu nome é:
-              <b-form-input v-model="payload.name" placeholder="">
-              </b-form-input
-              >.
-            </p>
-            <p>
-              Gostaria de falar sobre
-              <b-form-select v-model="dept" :options="options" id="dept">
-              </b-form-select>
-              com vocês.
-            </p>
-
-            <div class="services" v-if="dept">
-              <p v-if="dept === 'rh'">
-                Para conhecer nossas vagas, <a href="#">Clique aqui.</a> Agora
-                se precisar de outras informações, continua a preencher o
-                formulário. :)
-              </p>
-
-              <p>
-                Meu e-mail é
-                <b-form-input v-model="payload.email" placeholder="">
-                </b-form-input>
-                e
-              </p>
-              <p>
-                meu telefone é
-                <b-form-input v-model="payload.phone" placeholder="">
-                </b-form-input
-                >.
-              </p>
-              <p>Informações adicionais:</p>
-              <b-form-textarea
-                id="textarea"
-                v-model="payload.extra"
-                placeholder="Digite alguma mensagem"
-                rows="3"
-              ></b-form-textarea>
+    <div class="container">
+        <div class="row laptop-brq">
+            <div class="col-lg-5">
+                <h2 class="blue-text">blog brq</h2>
+                <p>Aqui você encontra os principais conteudos sobre transformação digital e tencologia autalizados semanalmente além de cases e-books e infogréficos.</p>
+                <h4 class="blue-text">Acesse!</h4>
             </div>
-
-            <b-button
-              class="btn btn-primary mt-5"
-              type="submit"
-              :disabled="!dept"
-              >Enviar <img src="~@/static/images/arrow-dobra.svg" alt=""
-            /></b-button>
-          </div>
-          <!--end of form content-->
-          <div class="col-md-3 side-box mt-5 mx-4 mx-md-0 mt-md-0">
-            <div class="content">
-              <h2>Conheça também nossos canais digitais</h2>
-              <ul class="list-unstyled">
-                <li class="py-3">
-                  <a
-                    class=""
-                    href="https://www.facebook.com/brqdigital/"
-                    target="_blank"
-                  >
-                    <img src="~@/static/images/facebook.svg" alt="" />
-                    Facebook
-                  </a>
-                </li>
-                <li class="py-3">
-                  <a
-                    class=""
-                    href="https://twitter.com/brqdigital"
-                    target="_blank"
-                    ><img src="~@/static/images/twitter.svg" alt="" />
-                    Twitter</a
-                  >
-                </li>
-                <li class="py-3">
-                  <a
-                    class=""
-                    href="https://www.youtube.com/user/brqitservices"
-                    target="_blank"
-                    ><img src="~@/static/images/youtube.svg" alt="" />
-                    Youtube</a
-                  >
-                </li>
-                <li class="py-3">
-                  <a
-                    class=""
-                    href="https://www.linkedin.com/company/brq/?originalSubdomain=br"
-                    target="_blank"
-                    ><img src="~@/static/images/linkedin.svg" alt="" />
-                    linkedin</a
-                  >
-                </li>
-                <li class="py-3">
-                  <a
-                    class=""
-                    href="https://www.instagram.com/brqdigital/"
-                    target="_blank"
-                    ><img src="~@/static/images/instagram.svg" alt="" />
-                    Instagram</a
-                  >
-                </li>
-              </ul>
+            <div class="col-lg-7 laptop">
+              <img src="~@/static/images/laptop-brq.png"/>
             </div>
-          </div>
+            <div class="col-lg-6 p-0">
+            </div>
         </div>
-      </div>
-    </section>
-
-    <section id="where" class="bg-diagonal-light">
+    </div>
+    <section class="bg-diagonal-light" id="ultimas-noticias">
       <div class="container content">
         <div class="row">
-          <div class="col-md-12 text-md-right mt-5 mt-md-0 py-5">
-            <h2 class="blue-title">Onde Estamos</h2>
+          <div class="col-6" id="foto">
+            <img src="~@/static/images/foto-noticias.png"/>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-md-2 mt-3">
-            <h4>Matriz - SP</h4>
-            <p>
-              Rua Boa Vista, 254 <br />
-              9 andar <br />Centro<br />
-              São Paulo - SP<br />
-              CEP 01014-000 <br />+55 (11) 2526-0600
-            </p>
-          </div>
-          <div class="col-md-2 mt-3">
-            <h4>Alphaville - SP</h4>
-            <p>
-              Av Copacabana,238 <br />
-              3 andar <br />18 do Forte<br />
-              Barueri - SP<br />
-              CEP 06465-903 <br />+55 (11) 3927-0600
-            </p>
-          </div>
-          <div class="col-md-2 mt-3">
-            <h4>Rio de Janeiro - RJ</h4>
-            <p>
-              Rua Teofilo Otoni,63 <br />
-              4 andar <br />Centro<br />
-              Rio de Janeiro - RJ<br />
-              CEP 20090-080<br />+55 (21) 3211-9800 / +55 (21) 3211-9811
-            </p>
-          </div>
-          <div class="col-md-2 mt-3">
-            <h4>Curitiba - PR</h4>
-            <p>
-              Av. Mal. Floriano Peixoto, 2610 <br />
-              Cj.07 <br />Parolin<br />
-              Curitiba - PR<br />
-              CEP 80220-000 <br />+55 (41) 3778-8000 <br />
-              +55 (41) 3778-8002
-            </p>
-          </div>
-          <div class="col-md-2 mt-3">
-            <h4>Recife - PE</h4>
-            <p>
-              Av. Marquês de Olinda, 126 <br />
-              Saka 102 - 1 andar Condomínio Citi Empresarial <br />Bairro do
-              Recife<br />
-              Recife - PE<br />
-              CEP 50030-000
-            </p>
-          </div>
-          <div class="col-md-2 mt-3">
-            <h4>New York - USA</h4>
-            <p>
-              20 Hicksville Road <br />
-              Suite 7 <br />Massapequa<br />
-              New York 11758 <br />+1 516-541 3100
-            </p>
+          <div class="col-lg-5 offset-lg-1 my-5 text-right ultimas-noticias">
+            <h2 class="blue-text pb-5">últimas notícias</h2>
+            <p class="pb-5">acompanhe os releases enviados, notíciaas na omprensa e demais informações</p>
+          
+            <button class="btn btn-primary">
+              <div class="row">
+                <div class="col-8 font-desc text-left">
+                  Confira as últimas notícias sobre a BRQ
+                </div>
+                <div class="col-4">
+                  <img src="~@/static/images/arrow-dobra.svg" height="100%" class="image-seta">
+                </div>
+              </div>
+            </button>
           </div>
         </div>
       </div>
     </section>
+
+    <section class="py-5 py-md-3">
+      <div class="container">
+        <p>Para mais informações, solicitações de entrevistas e conteúdos<br>entre em contato com <span>Leticia@pineapplehub.com.br</span><br>ou <span>marketing@brq.com</span></p>
+      </div>
+    </section>
+
   </div>
 </template>
 
@@ -216,45 +84,6 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default {
-  data: () => ({
-    dept: "",
-    payload: {
-      name: "",
-      email: "",
-      phone: "",
-      extra: "",
-    },
-    options: [
-      {
-        value: null,
-        text: "",
-      },
-      {
-        value: "services",
-        text: "Serviços e Soluções",
-      },
-      {
-        value: "rh",
-        text: "Recursos Humanos",
-      },
-      {
-        value: "press",
-        text: "Imprensa",
-      },
-      {
-        value: "ri",
-        text: "Relação com Investidores",
-      },
-      {
-        value: "innovation",
-        text: "Innovation Hub",
-      },
-      {
-        value: "suppliers",
-        text: "Fornecedores",
-      },
-    ],
-  }),
   mounted() {
     gsap.to(".diagonal-pages", {
       scrollTrigger: {
@@ -271,7 +100,75 @@ export default {
 
 
 <style lang="scss" scoped>
+
+.font-desc {
+  font-size: 1.3rem;
+}
+
+#foto {
+  position: relative;
+  width: 100%;
+  img {
+    position: absolute;
+    width: 100%;
+    transform: translateY(-5%);
+  }
+}
+
+#ultimas-noticias {
+  padding-bottom: 7rem;
+  font-size: 1.3rem;
+}
+
 .main-title {
   padding-bottom: 10rem;
+}
+
+h1 {
+    font-size: 4.5rem;
+    text-transform: uppercase;
+    font-weight: bolder;
+    color: var(--white);
+}
+
+.laptop {
+  position: relative;
+  img {
+    position: absolute;
+    top: -130%;
+    width: 100%;
+  }
+}
+
+h2 {
+    font-size: 3rem;
+    text-transform: uppercase;
+    font-weight: bolder;
+}
+
+.blue-text {
+    color: var(--brq-darkblue);
+}
+
+.white-text {
+    color: var(--white);
+}
+
+.laptop-brq {
+  margin-bottom: 20rem;
+  font-size: 1.3rem;
+}
+
+@media (max-width: 575px) {
+  .laptop-brq {
+    flex-direction: column-reverse;
+  }
+
+  .laptop {
+  position: relative;
+  img {
+    position: relative;
+  }
+}
 }
 </style>
