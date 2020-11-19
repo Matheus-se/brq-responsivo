cbf54e08ed401669c434aafddc5d6711f22140ff
miguelmendes44 <miguel.mendes@bitzen.com.br>
2020-11-19 13:58:00 -0300

    Changes Responsive

--- components/Header.vue
+++ components/Header.vue
@@ -20,8 +20,7 @@
               fill-rule="evenodd"
               d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
           />
-        </svg
-        >
+        </svg>
       </b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
