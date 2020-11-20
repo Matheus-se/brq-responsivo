c3bb34c0c05e4d72efc14509ebca6acfd53b6ff3
Felippe Kanashiro <felippe@sioux.com.br>
2020-11-19 22:05:43 -0300

    * configuração de deploy * ajuste de favicon * ajuste de title

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
     },
     // Global page headers (https://go.nuxtjs.dev/config-head)
     head: {
-        title: 'brq-bt',
+        title: 'BRQ Digital Solutions',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -19,7 +19,7 @@ export default {
                 rel: 'stylesheet',
                 href: 'https://fonts.googleapis.com/css?family=Bebas+Neue'
             },
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            { rel: 'icon', type: 'image/png', href: '/favicon.png' }
         ]
     },
 
